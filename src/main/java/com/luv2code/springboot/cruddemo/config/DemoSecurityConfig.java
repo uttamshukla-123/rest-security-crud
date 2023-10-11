617c3d7f7149fefd39216a48e625695d165540ab
Uttam2021 <uttamshukla12345@gmail.com>
2023-10-11 12:34:03 +0530

    used JDBCauthentication

--- src/main/java/com/luv2code/springboot/cruddemo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/config/DemoSecurityConfig.java
@@ -10,18 +10,48 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for JDBC  no more hardcoded users
+
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+         return  new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests(configure->
+                configure.requestMatchers(HttpMethod.GET,"/api/employees" ).hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/api/employees/**" ).hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/employees" ).hasRole("MANAGER")
+                        . requestMatchers(HttpMethod.PUT,"/api/employees" ).hasRole("MANAGER")
+                        . requestMatchers(HttpMethod.DELETE,"/api/employees/**" ).hasRole("ADMIN"));
+
+        //user http basic authentication
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        //disable CSRF
+        // in general not required for Stateless Rest APIs that uses  GET,POST,PUT and DELETE
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        return httpSecurity.build();
+    }
+
+ /*   @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails john= User.builder().username("john").password("{noop}testjohn").roles("EMPLOYEE").build();
         UserDetails mary= User.builder().username("mary").password("{noop}testmary").roles("EMPLOYEE","MANAGER").build();
 
         UserDetails susan= User.builder().username("susan").password("{noop}testsusan").roles("EMPLOYEE","MANAGER", "ADMIN").build();
-            return  new InMemoryUserDetailsManager(john,mary,susan);
+        return  new InMemoryUserDetailsManager(john,mary,susan);
 
     }
 
@@ -43,4 +73,6 @@ public class DemoSecurityConfig {
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
     }
+    */
 }
+
